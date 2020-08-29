30adf4a8a95b3d651836834a3edf0e018a9f6eee
selam lemma <oneiconforlife26@gmail.com>
2020-08-29 14:12:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -12,33 +12,24 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
-
-
 	programmer_hash =
  		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
     :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+        {:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+        :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
+        :dennis_ritchie => {:known_for => "Unix",:languages => "C"
+        ##:yukihiro_matsumoto=>{:known_for=>"Ruby",:languages=> ["LISP", "C"]
         }
      }
+        programmer_hash[:dennis_ritchie][:languages]="C"
 end
 
 def adding_matz
@@ -47,61 +38,35 @@ def adding_matz
    #:known_for => "Ruby",
   # :languages => ["LISP", "C"]
 # }
-
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+     {:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+     :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
-
-programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby",:languages => ["LISP", "C"]}]
+      programmer_hash[:yukihiro_matsumoto]={}
+      programmer_hash[:yukihiro_matsumoto][:known_for] ="Ruby"
+      programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP","C"]
+      programmer_hash
 end
 
 def changing_alan
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
+ 		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+     :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
-     programmer_hash[:alan_kay][:known_for]="GUI"
+    programmer_hash[:alan_kay][:known_for]="GUI"
+   programmer_hash
      #alans_new_info = "GUI"
-
-
 end
+
 
 def adding_to_dennis
 	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => "C"
-        }
+ 		{:grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
+     :alan_kay => {:known_for => "Object Orientation",:languages => ["Smalltalk", "LISP"]},
+     :dennis_ritchie => {:known_for => "Unix",:languages => ["C"]}
      }
+      programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+      programmer_hash
+end
